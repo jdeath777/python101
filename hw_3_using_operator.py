2f0b5e8e7e7ec65dca84324df8e95f823b1eb7b2
jdeath777 <jithin.abraham3@gmail.com>
2018-09-15 20:46:34 +0530

    Operators & Squareroot

---
+++ hw_3_using_operator.py
@@ -0,0 +1,28 @@
+print("Exercise Using operators")
+
+print("First",25 + 30 / 6)
+
+print("Second", 100 - 25 * 3 % 4)
+
+print("Third", 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Fourrth", 3 + 2 < 5 - 7)
+
+print("3 + 2?", 3+2)
+
+print("5 - 7?",5-7)
+
+print("IS it greater?",5>-2)
+
+print("is it greater or equal? 5>=2", 5>=2)
+
+print("Is less or equal 5<= -2", 5 <= -2)
+
+print("True or False, 3<5", 3<5)
+
+print("Square Root Operation,square root of 64 ",64 **(1/2))
+
+print("Square root of 81", 81**(1/2))
+
+
+
