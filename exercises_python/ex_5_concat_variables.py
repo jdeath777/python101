8657287e54ac89cc50472b66450f6d0d94793e79
jdeath777 <jithin.abraham3@gmail.com>
2018-09-23 06:33:39 +0530

    end' ' paremeter

---
+++ exercises_python/ex_5_concat_variables.py
@@ -0,0 +1,35 @@
+#print statement with concatenation
+
+
+
+
+print("Mary had a little lamb.")
+print("And everywhere that Mary went.")
+
+#print the "." 10 times to forrm a line
+print("." * 10) 
+
+#declare values to variables
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print("Excepted output: Cheese \nBurger")
+
+print("Actual output")
+
+# The end parameter helps python to not stop at a new line
+#it tells python to continue in the same line
+#The space inside ' ' of end gives the space between the two line 
+#or add a '\t' for space
+print(end1 + end2 + end3 + end4 + end5 + end6,end='\t')
+print(end7 + end8 + end9 + end10 + end11 + end12)
