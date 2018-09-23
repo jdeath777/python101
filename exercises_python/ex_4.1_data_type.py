2a9846ba298e86e0560da40935f6d1b4f329f54e
jdeath777 <jithin.abraham3@gmail.com>
2018-09-23 06:32:17 +0530

    data types

---
+++ exercises_python/ex_4.1_data_type.py
@@ -0,0 +1,22 @@
+#Different data types
+
+#An integer data type
+count = 10
+
+#A floating point
+miles = 1000.0
+
+#A string data type
+name = "jithin"
+
+print("Expected outputs")
+
+print("10")
+print("1000.0")
+print("jithin")
+
+print("Actuall output")
+#print variables
+print(count)
+print(miles)
+print(name)
