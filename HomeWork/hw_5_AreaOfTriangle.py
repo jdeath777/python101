8312f95deb0629ed0fa18744bc1980b5fc705f07
jdeath777 <jithin.abraham3@gmail.com>
2018-09-26 23:44:01 +0530

    calculating float input

---
+++ HomeWork/hw_5_AreaOfTriangle.py
@@ -0,0 +1,16 @@
+#To calculate the area of a triangle
+
+#declare variable and take user input in them
+print("Enter the height of the triangle")
+
+#use float for input to specify the input type to multiply with a decimal later
+h = float(input())
+
+print("Enter the base height of the triangle")
+b = float(input())
+
+#do the area calculation
+a = 0.5 * h * b
+
+#pass the value of the area
+print("The area of the triangle is: ",a)
