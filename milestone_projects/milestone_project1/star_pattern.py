2c4250e2e8f4e1ca5b30fa57165bdc7df9ba4a14
jdeath777 <jithin.abraham3@gmail.com>
2018-10-04 07:35:29 +0530

    triangle pattern

---
+++ milestone_projects/milestone_project1/star_pattern.py
@@ -0,0 +1,11 @@
+a = '*'
+
+#loop for getting the rows
+
+for i  in range(0,10):
+    #loop for setting the columns
+    for j in range(0,i+1):
+        print(a, end = "")
+    #to end line after each row    
+    print("\r")
+
