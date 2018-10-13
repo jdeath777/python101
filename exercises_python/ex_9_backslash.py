8d97cacca9c972a7a5484fb7d0cbf2d7f5ae6c6a
jdeath777 <jithin.abraham3@gmail.com>
2018-10-13 20:58:26 +0530

    backlash

---
+++ exercises_python/ex_9_backslash.py
@@ -0,0 +1,18 @@
+#use of back slashes
+
+dog = "\tI'm in love with dogs."
+cat = "I'm split\non a line."
+black_cat = "I \\hate\\cats."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* Kill mouse\n\t* using paws
+"""
+
+print(dog)
+print(cat)
+print(black_cat)
+print(fat_cat)
+
