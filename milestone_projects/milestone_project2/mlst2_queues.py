197fd8a3c41f48dfe9e45717b6f95b062f4503fa
jdeath777 <jithin.abraham3@gmail.com>
2018-10-13 07:40:29 +0530

    queues

---
+++ milestone_projects/milestone_project2/mlst2_queues.py
@@ -0,0 +1,23 @@
+#implementing stack
+
+stack = input("Enter the name to be added: ")
+
+lst = stack.split(' ')
+
+
+b = input("Enter the new name to be added: ")
+
+c = lst.append(b)
+
+print(lst)
+
+i  = input("do you want to pop the item: ")
+
+if(i == 'y' or i == 'Y'):
+    lstack = ["Amar", "Akbar", "Anthony"]
+    p = lst.pop(0)
+    print("The popped item is: ",p)
+    print("The remaining item is: ")
+    print(lst)
+else:
+    print(lst)
