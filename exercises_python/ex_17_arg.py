f3e68dc01cd83aa4bf8cef520508e0b2b634966f
jdeath777 <jithin.abraham3@gmail.com>
2018-11-01 19:35:26 +0530

    arg1

---
+++ exercises_python/ex_17_arg.py
@@ -0,0 +1,12 @@
+import sys
+program_name = sys.argv[0]
+arguments = sys.argv[1:]
+count = len(arguments)
+
+
+from sys import argv
+script, first, second, third = argv
+print("The script is called:", script)
+print("The first variable is :", first)
+print("The second variable is :", second)
+print("Your third variable is:", third)
